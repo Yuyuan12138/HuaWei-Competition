48eaecb744dcf6509bd7f05e858216b723391da1
glerium <970656328@qq.com>
2024-03-09 11:03:56 +0800

    add fast read/write features

--- Input.cpp
+++ Input.cpp
@@ -4,6 +4,41 @@
 
 #include "Input.h"
 
+/**
+ * @brief 从标准输入快速读取一个整数
+ * @param[out] x 要读入的变量
+*/
+template<typename T> void read(T& x) {
+    int v = 0, w = 1;
+    char ch = getchar();
+    while (!isdigit(ch)) {
+        if(ch == '-')
+            w = -1;
+        ch = getchar();
+    }
+    while (isdigit(ch)) {
+        v = v * 10 + ch - '0';
+        ch = getchar();
+    }
+    x = v * w;
+}
+
+/**
+ * @brief 向标准输出快速写入一个整数
+ * @param[out] x 要写入的变量
+*/
+template<typename T> void write(T x) {
+    if (x < 0) {
+        putchar('-');
+        write(-x);
+    } else if (x < 10) {
+        putchar(x + '0');
+    } else {
+        write(x / 10);
+        putchar(x % 10 + '0');
+    }
+}
+
 int Input()
 {
     scanf("%d%d", &id, &money);
