7331f25f062f79184a4f9575274226ee3e5ea76e
glerium <970656328@qq.com>
2024-03-10 14:47:58 +0800

    polish code styles

--- Constants.h
+++ Constants.h
@@ -5,7 +5,7 @@
 #ifndef CODECRAFTSDK_CONSTANTS_H
 #define CODECRAFTSDK_CONSTANTS_H
 
-inline constexpr int n = 200;
+inline constexpr int n = 200;               /** 地图大小 */
 inline constexpr int robot_num = 10;        /** 机器人最大容量 */
 inline constexpr int berth_num = 10;        /** 港口最大容量 */
 inline constexpr int MAX_AREAS = 10;        /** 区域最大容量 */
--- Controller.h
+++ Controller.h
@@ -7,7 +7,7 @@
 #include <vector>
 
 struct Operation{
-    int objector;    /// 对象指代：0，robot。1，boat。
+    int objector;    /// 对象指代：0, robot. 1, boat.
     int command;
     int id;
     int optionArg;
@@ -19,7 +19,7 @@ struct Operation{
  *      // 存入数据
  *      operations.push_back(struct Operation);
  * 结构体参数
- * @param objector 0，robot。1，boat。
+ * @param objector 0, robot. 1, boat.
  * @param command For robot: 0, move. 1, get. 2, pull. For boat: 0, ship. 1, go.
  * @param id id of robot or boat.
  * @param optionArg For robot: 0, right. 1, left. 2, up. 3, down. For boat: the id of berth.
